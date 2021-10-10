af45d44ed7ce6d3454eff3e746d46b31ae9c0c55
paad112233@naver.com <paad112233@naver.com>
2021-10-11 01:56:08 +0900

    initial work about copy line panel listing

--- OneClickCopyButton/ClipboardData.cs
+++ OneClickCopyButton/ClipboardData.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace OneClickCopy
 {
     public class ClipboardData
     {
-        private string datatTitle = "Empty";
-        private string datatContents = "";
-
-        private bool isBinaryData = false;
+        private string title = "Empty";
+        private DataObject content = null;
     }
 }
